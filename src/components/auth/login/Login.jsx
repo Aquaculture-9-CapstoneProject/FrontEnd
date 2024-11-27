d835830315dab1349944f19ea8bb5626207d6d0b
alipnf <alipnf@proton.me>
2024-11-27 19:08:28 +0700

    refactor(auth): extract login title and logo to components

--- src/components/auth/login/Login.jsx
+++ src/components/auth/login/Login.jsx
@@ -1,32 +1,11 @@
-import { useState } from "react";
 import LoginDesktop from "./LoginDesktop";
 import LoginMobile from "./LoginMobile";
 
 export default function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log("Form submitted:", { email, password });
-  };
-
   return (
     <>
-      <LoginDesktop
-        handleSubmit={handleSubmit}
-        email={email}
-        setEmail={setEmail}
-        password={password}
-        setPassword={setPassword}
-      />
-      <LoginMobile
-        handleSubmit={handleSubmit}
-        email={email}
-        setEmail={setEmail}
-        password={password}
-        setPassword={setPassword}
-      />
+      <LoginDesktop />
+      <LoginMobile />
     </>
   );
 }
--- src/components/auth/login/LoginForm.jsx
+++ src/components/auth/login/LoginForm.jsx
@@ -1,12 +1,28 @@
-export default function LoginForm({
-  handleSubmit,
-  email,
-  setEmail,
-  password,
-  setPassword,
-}) {
+import { useForm } from "react-hook-form";
+import useAuthStore from "../../../store/useAuthStore";
+import { useNavigate } from "react-router-dom";
+
+export default function LoginForm() {
+  const { email, setEmail, password, setPassword } = useAuthStore();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const navigate = useNavigate();
+  const onSubmit = (data) => {
+    setEmail(data.email);
+    setPassword(data.password);
+    console.log("Form submitted:", data);
+  };
+
+  const handleForgotPassword = () => {
+    console.log("pergi ke lupa password");
+  };
+
   return (
-    <form onSubmit={handleSubmit} className="space-y-4">
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
       {/* Email */}
       <div className="form-control mt-10">
         <label htmlFor="email" className="label">
@@ -15,13 +31,24 @@ export default function LoginForm({
         <input
           id="email"
           type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          className="input input-bordered w-full"
-          required
+          placeholder="example@gmail.com"
+          defaultValue={email}
+          {...register("email", {
+            required: "Email wajib diisi.",
+            pattern: {
+              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+              message: "Format email tidak valid.",
+            },
+          })}
+          className={`input-style ${errors.email ? "input-error" : "border-secondary-5"}`}
         />
+        {errors.email && (
+          <p className="text-error-4 text-[10px] mt-1">
+            {errors.email.message}
+          </p>
+        )}
       </div>
+
       {/* Password */}
       <div className="form-control">
         <label htmlFor="password" className="label">
@@ -30,30 +57,43 @@ export default function LoginForm({
         <input
           id="password"
           type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          className="input input-bordered w-full"
-          required
+          placeholder="Minimal 6 karakter"
+          defaultValue={password}
+          {...register("password", {
+            required: "Password wajib diisi.",
+            minLength: {
+              value: 6,
+              message: "Password harus minimal 6 karakter.",
+            },
+          })}
+          className={`input-style ${errors.password ? "input-error" : "border-secondary-5"}`}
         />
+        {errors.password && (
+          <p className="text-error-4 text-[10px] mt-1">
+            {errors.password.message}
+          </p>
+        )}
       </div>
+
       {/* Lupa Password */}
-      <p className="text-[10px] text-neutral-1 text-right hover:text-primary-1 cursor-pointer">
-        Lupa password?
-      </p>
+      <div className="flex justify-between items-center">
+        <p className="forgot-password" onClick={handleForgotPassword}>
+          Lupa password?
+        </p>
+      </div>
 
       <div className="form-control mt-10">
         {/* Submit Button */}
-        <button
-          type="submit"
-          className="btn bg-primary-5 w-full font-bold text-base text-neutral-5 mt-6"
-        >
+        <button type="submit" className="button-style">
           Masuk
         </button>
         {/* Registrasi */}
         <p className="text-base text-center mt-6">
           Belum punya akun?{" "}
-          <span className="text-secondary-5 font-bold cursor-pointer">
+          <span
+            className="text-secondary-5 font-bold cursor-pointer"
+            onClick={() => navigate("/register")}
+          >
             Buat akun baru
           </span>
         </p>
