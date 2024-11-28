bf0bc4fb0768c157c8df664e30047b7822fa4165
alipnf <alipnf@proton.me>
2024-11-29 03:10:52 +0700

    refactor(auth): update logo and form styles

--- src/components/auth/register/RegisterDesktop.jsx
+++ src/components/auth/register/RegisterDesktop.jsx
@@ -1,12 +1,11 @@
 import RegisterForm from "./RegisterForm";
-import Logo from "../../common/Logo";
 import RegisterTitle from "./RegisterTitle";
 
 export default function RegisterDesktop() {
   return (
     <div className="hidden lg:flex h-screen w-full bg-[url('/bg-auth.png')] bg-cover bg-center">
       <div className="w-1/2 bg-white flex-col justify-center items-center px-28 py-16 rounded-r-[20px] overflow-y-auto">
-        <Logo />
+        <img className="mb-8" src="./logo.svg" />
         <RegisterTitle />
         <RegisterForm />
       </div>
--- src/components/auth/register/RegisterMobile.jsx
+++ src/components/auth/register/RegisterMobile.jsx
@@ -1,11 +1,10 @@
 import RegisterForm from "./RegisterForm";
-import Logo from "../../common/Logo";
 import RegisterTitle from "./RegisterTitle";
 
 export default function RegisterMobile() {
   return (
-    <div className="lg:hidden flex flex-col justify-center h-screen px-10 bg-white">
-      <Logo />
+    <div className="lg:hidden flex flex-col justify-center h-screen px-4 sm:px-6 md:px-10 bg-white">
+      <img className="mb-8 w-[126px]" src="./logo.svg" alt="Logo" />
       <RegisterTitle />
       <RegisterForm />
     </div>
