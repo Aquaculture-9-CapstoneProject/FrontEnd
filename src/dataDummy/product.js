344fb9ec95e4604d75803fc1999f05658b9bfbbb
alipnf <alipnf@proton.me>
2024-12-04 02:01:02 +0700

    feat(product): add dummy product data

---
+++ src/dataDummy/product.js
@@ -0,0 +1,38 @@
+export const products = [
+  {
+    id: 1,
+    name: "Ikan Salmon",
+    price: 40000,
+    image: "/user/home/card-ikan.png",
+    category: "ikan",
+    quantity: 1,
+    rating: 4.5,
+  },
+  {
+    id: 2,
+    name: "Ikan Tuna",
+    price: 35000,
+    image: "/user/home/card-ikan.png",
+    category: "ikan",
+    quantity: 1,
+    rating: 4.5,
+  },
+  {
+    id: 3,
+    name: "Ikan Lele",
+    price: 25000,
+    image: "/user/home/card-ikan.png",
+    category: "ikan",
+    quantity: 1,
+    rating: 4.0,
+  },
+  {
+    id: 4,
+    name: "Ikan Gurami",
+    price: 45000,
+    image: "/user/home/card-ikan.png",
+    category: "ikan",
+    quantity: 1,
+    rating: 4.7,
+  },
+];
