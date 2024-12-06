7d54ec1d4a770d34a524eb5bbf9d2a0620c6a100
alipnf <alipnf@proton.me>
2024-12-06 19:28:49 +0700

    feat(admin): add admin layout and pages

---
+++ src/pages/admin/TransactionPage.jsx
@@ -0,0 +1,10 @@
+import Transaction from "../../components/admin/transaction/Transaction";
+import AdminLayout from "./AdminLayout";
+
+export default function TransactionPage() {
+  return (
+    <AdminLayout>
+      <Transaction />
+    </AdminLayout>
+  );
+}
