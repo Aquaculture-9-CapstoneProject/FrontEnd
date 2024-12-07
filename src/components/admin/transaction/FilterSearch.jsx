402411ae15b9c33ad047906095b22087e1d01ad4
lintangisnandar <lintangisnandar@students.unnes.ac.id>
2024-12-07 14:02:52 +0700

    feat(transaction-table): add modal for deleting transaction

--- src/components/admin/transaction/FilterSearch.jsx
+++ src/components/admin/transaction/FilterSearch.jsx
@@ -1,56 +1,60 @@
 export default function FilterSearch() {
-    return (
-      <div className="flex justify-between items-center mb-4">
-        <h2 className="text-sm font-semibold text-neutral-1">Transaksi Bulan Ini</h2>
-        <div className="flex gap-2 items-center">
-          <div className="flex items-center border border-neutral-3 bg-neutral-5 rounded-md">
-            <input
-              type="text"
-              placeholder="Cari"
-              className="input input-sm bg-neutral-5 text-[10.35px] border-0 focus:outline-none"
-            />
-            <button className="px-2">
-              <svg
-                width="14"
-                height="14"
-                viewBox="0 0 14 14"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M10.191 9.03627H9.60672L9.39964 8.83658C10.2871 7.80117 10.7457 6.38856 10.4942 4.88721C10.1466 2.83117 8.43079 1.18929 6.35995 0.937832C3.23151 0.553249 0.598598 3.18617 0.983182 6.3146C1.23464 8.38544 2.87652 10.1013 4.93256 10.4489C6.43391 10.7003 7.84651 10.2418 8.88193 9.35429L9.08162 9.56137V10.1456L12.2248 13.2889C12.5281 13.5921 13.0236 13.5921 13.3268 13.2889C13.6301 12.9856 13.6301 12.4901 13.3268 12.1869L10.191 9.03627ZM5.75349 9.03627C3.91193 9.03627 2.42537 7.54971 2.42537 5.70815C2.42537 3.86658 3.91193 2.38002 5.75349 2.38002C7.59506 2.38002 9.08162 3.86658 9.08162 5.70815C9.08162 7.54971 7.59506 9.03627 5.75349 9.03627Z"
-                  fill="#A3A3A3"
-                />
-              </svg>
-            </button>
-          </div>
-          <details className="dropdown dropdown-end">
-            <summary className="btn btn-sm bg-neutral-5 border-neutral-4 text-neutral-1 text-xs font-semibold flex items-center gap-2">
-              <svg
-                width="12"
-                height="8"
-                viewBox="0 0 12 8"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M4.66667 8H7.33333V6.66667H4.66667V8ZM0 0V1.33333H12V0H0ZM2 4.66667H10V3.33333H2V4.66667Z"
-                  fill="#262626"
-                />
-              </svg>
-              Filter
-            </summary>
-            <ul className="mt-2 menu dropdown-content bg-neutral-5 text-xs rounded-box z-[1] w-40 p-2 shadow">
-              <li>
-                <a>Berhasil</a>
-              </li>
-              <li>
-                <a>Gagal</a>
-              </li>
-            </ul>
-          </details>
+  return (
+    <div className="flex justify-between items-center mb-4 flex-wrap sm:flex-nowrap">
+      <h2 className="text-sm sm:text-base font-semibold text-neutral-1 mb-2 sm:mb-0">
+        Transaksi Bulan Ini
+      </h2>
+      <div className="flex gap-2 items-center w-full sm:w-auto">
+        {/* Input Pencarian */}
+        <div className="flex items-center border border-neutral-3 bg-neutral-5 rounded-md w-full sm:w-[200px]">
+          <input
+            type="text"
+            placeholder="Cari"
+            className="input input-sm bg-neutral-5 text-[10.35px] sm:text-sm border-0 focus:outline-none w-full py-2 px-3"
+          />
+          <button className="px-2">
+            <svg
+              width="14"
+              height="14"
+              viewBox="0 0 14 14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M10.191 9.03627H9.60672L9.39964 8.83658C10.2871 7.80117 10.7457 6.38856 10.4942 4.88721C10.1466 2.83117 8.43079 1.18929 6.35995 0.937832C3.23151 0.553249 0.598598 3.18617 0.983182 6.3146C1.23464 8.38544 2.87652 10.1013 4.93256 10.4489C6.43391 10.7003 7.84651 10.2418 8.88193 9.35429L9.08162 9.56137V10.1456L12.2248 13.2889C12.5281 13.5921 13.0236 13.5921 13.3268 13.2889C13.6301 12.9856 13.6301 12.4901 13.3268 12.1869L10.191 9.03627ZM5.75349 9.03627C3.91193 9.03627 2.42537 7.54971 2.42537 5.70815C2.42537 3.86658 3.91193 2.38002 5.75349 2.38002C7.59506 2.38002 9.08162 3.86658 9.08162 5.70815C9.08162 7.54971 7.59506 9.03627 5.75349 9.03627Z"
+                fill="#A3A3A3"
+              />
+            </svg>
+          </button>
         </div>
+
+        {/* Dropdown Filter */}
+        <details className="dropdown dropdown-end">
+          <summary className="btn btn-sm bg-neutral-5 border-neutral-4 text-neutral-1 text-xs sm:text-sm font-semibold flex items-center gap-2">
+            <svg
+              width="12"
+              height="8"
+              viewBox="0 0 12 8"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M4.66667 8H7.33333V6.66667H4.66667V8ZM0 0V1.33333H12V0H0ZM2 4.66667H10V3.33333H2V4.66667Z"
+                fill="#262626"
+              />
+            </svg>
+            Filter
+          </summary>
+          <ul className="mt-2 menu dropdown-content bg-neutral-5 text-xs sm:text-sm rounded-box z-[1] w-40 p-2 shadow">
+            <li>
+              <a>Berhasil</a>
+            </li>
+            <li>
+              <a>Gagal</a>
+            </li>
+          </ul>
+        </details>
       </div>
-    );
-  }
-  
+    </div>
+  );
+}
--- src/components/admin/transaction/TransactionTableBody.jsx
+++ src/components/admin/transaction/TransactionTableBody.jsx
@@ -1,42 +1,41 @@
-export default function TransactionTableBody({ transactions }) {
-    return (
-      <tbody>
-        {transactions.map((transaction, index) => (
-          <tr key={index}>
-            <td className="text-neutral-2 text-xs border-b border-neutral-4">{transaction.id}</td>
-            <td className="text-neutral-2 text-xs border-b border-neutral-4">{transaction.orderId}</td>
-            <td className="text-neutral-2 text-xs border-b border-neutral-4">{transaction.date}</td>
-            <td className="text-neutral-2 text-xs border-b border-neutral-4">{transaction.paymentMethod}</td>
-            <td>
-              <span
-                className={`badge ${
-                  transaction.status === "Berhasil"
-                    ? "badge-success bg-neutral-5 border border-secondary-5 text-secondary-5 text-[10px]"
-                    : "badge-error bg-neutral-5 border border-error-3 text-error-3 text-[10px]"
-                }`}
+export default function TransactionTableBody({ transactions, openModal }) {
+  return (
+    <tbody>
+      {transactions.map((transaction, index) => (
+        <tr key={index}>
+          <td className="text-neutral-2 text-xs border-b border-neutral-4">{transaction.id}</td>
+          <td className="text-neutral-2 text-xs border-b border-neutral-4">{transaction.orderId}</td>
+          <td className="text-neutral-2 text-xs border-b border-neutral-4">{transaction.date}</td>
+          <td className="text-neutral-2 text-xs border-b border-neutral-4">{transaction.paymentMethod}</td>
+          <td>
+            <span
+              className={`badge ${
+                transaction.status === "Berhasil"
+                  ? "badge-success bg-neutral-5 border border-secondary-5 text-secondary-5 text-[10px]"
+                  : "badge-error bg-neutral-5 border border-error-3 text-error-3 text-[10px]"
+              }`}
+            >
+              {transaction.status}
+            </span>
+          </td>
+          <td>
+            <button onClick={() => openModal(transaction)}>
+              <svg
+                width="13"
+                height="16"
+                viewBox="0 0 13 16"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                {transaction.status}
-              </span>
-            </td>
-            <td>
-              <button>
-                <svg
-                  width="13"
-                  height="16"
-                  viewBox="0 0 13 16"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M1.20003 13.8333C1.20003 14.75 1.95003 15.5 2.8667 15.5H9.53337C10.45 15.5 11.2 14.75 11.2 13.8333V5.5C11.2 4.58333 10.45 3.83333 9.53337 3.83333H2.8667C1.95003 3.83333 1.20003 4.58333 1.20003 5.5V13.8333ZM11.2 1.33333H9.1167L8.52503 0.741667C8.37503 0.591667 8.15837 0.5 7.9417 0.5H4.45837C4.2417 0.5 4.02503 0.591667 3.87503 0.741667L3.28337 1.33333H1.20003C0.741699 1.33333 0.366699 1.70833 0.366699 2.16667C0.366699 2.625 0.741699 3 1.20003 3H11.2C11.6584 3 12.0334 2.625 12.0334 2.16667C12.0334 1.70833 11.6584 1.33333 11.2 1.33333Z"
-                    fill="#BE123C"
-                  />
-                </svg>
-              </button>
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    );
-  }
-  
+                <path
+                  d="M1.20003 13.8333C1.20003 14.75 1.95003 15.5 2.8667 15.5H9.53337C10.45 15.5 11.2 14.75 11.2 13.8333V5.5C11.2 4.58333 10.45 3.83333 9.53337 3.83333H2.8667C1.95003 3.83333 1.20003 4.58333 1.20003 5.5V13.8333ZM11.2 1.33333H9.1167L8.52503 0.741667C8.37503 0.591667 8.15837 0.5 7.9417 0.5H4.45837C4.2417 0.5 4.02503 0.591667 3.87503 0.741667L3.28337 1.33333H1.20003C0.741699 1.33333 0.366699 1.70833 0.366699 2.16667C0.366699 2.625 0.741699 3 1.20003 3H11.2C11.6584 3 12.0334 2.625 12.0334 2.16667C12.0334 1.70833 11.6584 1.33333 11.2 1.33333Z"
+                  fill="#BE123C"
+                />
+              </svg>
+            </button>
+          </td>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
