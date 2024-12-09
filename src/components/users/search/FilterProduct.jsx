71a182c19051086e5cb34560ab382c59ad36f017
alipnf <alipnf@proton.me>
2024-12-10 01:51:14 +0700

    refactor(search): update search page responsiveness

--- src/components/users/search/FilterProduct.jsx
+++ src/components/users/search/FilterProduct.jsx
@@ -12,8 +12,8 @@ export default function FilterProduct() {
     : products;
 
   return (
-    <div className="w-9/12">
-      <h1 className="text-lg">
+    <div className="w-full md:w-9/12">
+      <h1 className="text-lg text-center md:text-left">
         {selectedCategories.length > 0 ? (
           <>
             Menampilkan hasil pencarian untuk{" "}
@@ -23,24 +23,28 @@ export default function FilterProduct() {
           <>
             <span className="font-bold">Semua Produk</span>
           </>
-        )}{" "}
+        )}
       </h1>
 
       {filteredProducts.length > 0 ? (
-        <div className="grid grid-cols-3 gap-10 mt-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-10 mt-6">
           {filteredProducts.map((product) => (
-            <ProductCard
-              key={product.id}
-              name={product.name}
-              price={product.price}
-              image={product.image}
-              rating={product.rating}
-              category={product.category}
-            />
+            <div key={product.id} className="w-full max-w-[300px] mx-auto">
+              <ProductCard
+                key={product.id}
+                name={product.name}
+                price={product.price}
+                image={product.image}
+                rating={product.rating}
+                category={product.category}
+              />
+            </div>
           ))}
         </div>
       ) : (
-        <p className="text-gray-500 mt-4">Tidak ada produk ditemukan.</p>
+        <p className="text-gray-500 mt-4 text-center md:text-left">
+          Tidak ada produk ditemukan.
+        </p>
       )}
     </div>
   );
