c03f58b7fbd96cf72fe87e96a95cbf1ccd41cb6a
alipnf <alipnf@proton.me>
2024-12-10 16:29:31 +0700

    refactor(order): improve order page responsiveness

--- src/components/users/order/Order.jsx
+++ src/components/users/order/Order.jsx
@@ -5,10 +5,10 @@ export default function Order() {
   return (
     <>
       <Navbar />
-      <div className="flex justify-between items-center mx-16 my-4">
-        <h1 className="font-semibold text-[28px]">Daftar Pesanan</h1>
-        <div className="flex items-center">
-          <label className="input h-10 input-bordered flex items-center gap-2">
+      <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center mx-4 sm:mx-16 my-4">
+        <h1 className="font-semibold text-lg sm:text-[28px]">Daftar Pesanan</h1>
+        <div className="mt-4 sm:mt-0 flex items-center">
+          <label className="input h-10 input-bordered flex items-center gap-2 w-full sm:w-auto">
             <input
               type="text"
               className="grow text-sm"
@@ -22,11 +22,11 @@ export default function Order() {
             >
               <path
                 fillRule="evenodd"
-                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+                d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1-7 0Z"
                 clipRule="evenodd"
               />
             </svg>
-          </label>{" "}
+          </label>
         </div>
       </div>
       <OrderCard />
