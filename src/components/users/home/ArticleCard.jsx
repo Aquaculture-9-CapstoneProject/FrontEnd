2a41792ab6af85aafb64e12d93a864192d6f3ef3
alipnf <alipnf@proton.me>
2024-12-16 23:41:16 +0700

    fix(article-card): updates article card styling

--- src/components/users/home/ArticleCard.jsx
+++ src/components/users/home/ArticleCard.jsx
@@ -1,13 +1,16 @@
 export default function ArticleCard({ title, category, image, onClick }) {
   return (
     <div
-      className="rounded-[20px] relative h-[200px] w-[360px] bg-cover bg-no-repeat bg-center cursor-pointer transform transition-transform duration-200 ease-in-out hover:scale-105 active:scale-95"
-      style={{
-        backgroundImage: `linear-gradient(224deg, rgba(0, 0, 0, 0) 21.94%, rgba(0, 0, 0, 0.32) 56.76%), url(${image})`,
-      }}
+      className="rounded-[20px] relative h-[200px] min-w-[360px] w-full max-w-[400px] overflow-hidden cursor-pointer transform transition-transform duration-200 ease-in-out hover:scale-105 active:scale-95"
       onClick={onClick}
     >
-      <div className="flex flex-col justify-between h-full px-4 py-4">
+      <img
+        src={image}
+        alt={title}
+        className="absolute top-0 left-0 w-full h-full object-cover"
+      />
+      <div className="absolute inset-0 bg-gradient-to-b from-transparent to-black opacity-60"></div>
+      <div className="flex flex-col justify-between h-full px-4 py-4 relative z-10">
         <p className="font-semibold text-xs w-fit text-neutral-5 bg-secondary-5 px-2 py-1 rounded-xl">
           {category}
         </p>
